6292eb4b75af740e0fdc3c57102c5c50490c0c9e
Anirudha0313 <anirudhsuryawanshi03@gmail.com>
2026-03-09 21:24:04 +0530

    Reverse array added-Two pointer

---
+++ src/phase1_arrays/module2_two_pointers/ReverseArray.java
@@ -0,0 +1,33 @@
+package phase1_arrays.module2_two_pointers;
+
+public class ReverseArray {
+
+    // Reverse entire array using two pointers
+    public static void reverse(int[] arr) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        reverse(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
